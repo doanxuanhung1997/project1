870c527f98eb7a2c10f570c2e727ff514483589b
HungDX <doanxuanhung1997@gmail.com>
2022-05-18 09:50:47 +0700

    clear

--- api/sys_master/model/model.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type SysProductType struct {
-	tableName struct{} `pg:"public.sys_product_type"`
-
-	Id   int    `json:"id" pg:"type:serial,pk"`
-	Name string `json:"name" pg:",notnull"`
-}
--- helpers/constant/constant.go
+++
@@ -1,6 +0,0 @@
-package constant
-
-const (
-	DateFormat           = "2006-01-02"
-	DateTimeFormat       = "2006-01-02 15:04:05"
-)
